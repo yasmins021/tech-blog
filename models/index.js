eb9c08a714926e8efc65d932fd8547d02b0eafde
yasmins021 <109712172+yasmins021@users.noreply.github.com>
2023-05-12 14:13:33 -0700

    created db models

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Define sequelize associations in this file.
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+
+module.exports = { User, Post, Comment };
+
+
